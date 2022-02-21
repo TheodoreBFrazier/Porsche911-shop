5de5bf16475e57af22f5fd7e2a2e1c2f061dac9c
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-21 01:40:16 -0500

    Added a hero image of all Porsche generations

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -3,6 +3,7 @@ function Home() {
       <div className="Home">
         <h2>Welcome</h2>
         <h3>To the Only devoted Porsche 911 page!</h3>
+        <img src="https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/porsche_newsroom/Produkte/911/1-Mio.-911/b-CZ16MOD0007_rgb.jpg/jcr:content/b-CZ16MOD0007_rgb.jpg"/>
       </div>
     )
   }
