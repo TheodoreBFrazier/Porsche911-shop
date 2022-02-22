84fcec1cf09164464bf28774d3d0d128e7c4f4b7
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-22 02:32:37 -0500

    Deleted prod files and was having issues with heroku

---
+++ back-end/db/prod_schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS porsche_dev;
+CREATE DATABASE porsche_dev;
+
+\c porsche_dev;
+
+CREATE TABLE porsches (
+    id SERIAL PRIMARY KEY, 
+    name TEXT, 
+    image TEXT,
+    year INTEGER,
+    color TEXT,
+    generation INTEGER,
+    price INTEGER,
+    description TEXT,
+    featured BOOLEAN,
+    is_new BOOLEAN,
+    rating INTEGER
+);
+
+DROP TABLE IF EXISTS generations;
+
+CREATE TABLE generations (
+   id SERIAL PRIMARY KEY, 
+   generation TEXT,
+   image TEXT,
+   beganProd INTEGER,
+   endedProd INTEGER,
+   description TEXT    
+)
