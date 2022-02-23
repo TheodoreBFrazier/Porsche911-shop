10b10737ca743723f559e6957d44ce0f81274a4c
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-23 13:56:20 -0500

    EXACT path worked. Now importing the PorscheDetails

--- front-end/src/Pages/Show.js
+++ front-end/src/Pages/Show.js
@@ -1,8 +1,10 @@
-
+import PorscheDetails from "../Components/PorscheDetails";
+//make sure to use exact path to disable partial matching when displaying and index
 function Show() {
   return (
     <div className="show">
       <h2>Show</h2>
+      <PorscheDetails />
     </div>
   )
 }
