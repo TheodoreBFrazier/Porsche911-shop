712b4adcf994ac38677ac879c25b0d27a08ffd28
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-23 10:28:26 -0500

    Change import

--- front-end/src/Components/PorschesForSale.js
+++ front-end/src/Components/PorschesForSale.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Porsche4Sale } from "./Porsche4Sale";
+import  Porsche4Sale from "./Porsche4Sale";
 
 const API = process.env.REACT_APP_API_URL;
 
