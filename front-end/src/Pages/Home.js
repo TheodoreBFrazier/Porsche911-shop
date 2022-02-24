8f5bd28884b1833db3d8223ed76860412c92672a
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-24 04:04:50 -0500

    Committing CSS

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
         <p>A website dedicated to my favorite car <br /> one of the most famous cars of all time.</p>
         <h1>The Porsche 911</h1>
         <button className="genLink">
-          <Link to="/porsches/generations">Hisotry of the 911</Link>
+          <Link to="/porsches/generations">History of the 911</Link>
         </button>
       </div>
       <img src="https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/porsche_newsroom/Produkte/911/1-Mio.-911/b-CZ16MOD0007_rgb.jpg/jcr:content/b-CZ16MOD0007_rgb.jpg" alt="All porsche gens" />
