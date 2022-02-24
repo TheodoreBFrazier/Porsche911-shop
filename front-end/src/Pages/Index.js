958f1344e62e86fa7aba40673834616bb4014324
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-24 04:10:25 -0500

    more CSS

--- front-end/src/Pages/Index.js
+++ front-end/src/Pages/Index.js
@@ -5,7 +5,8 @@ function Index() {
         <div>
         <div className="Index-intro">
             <h2>Porsches for Sale</h2>
-            <p>We have several top quality porsche 911s available for sale. Take a look below and see what we have to offer!</p>
+            <p>There are several top quality used 911s for sale. Take a look below and see what we have to offer! <br/>
+            all these models are pre used, but gentley used and vetted for quality. One of the best entries to 911 overership ever!</p>
             <PorschesForSale />
         </div>
         </div>
